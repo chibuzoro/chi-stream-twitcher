d0dba220c0792db1ba7ab3881707dcfcff01f438
Chibuzor Ogbu <chibuzorogbu@gmail.com>
2019-04-08 12:17:23 +0100

    built frontend components - homepage

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,14 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import Vue from 'vue';
+import './plugins/vuetify';
+import App from './App.vue';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import router from './router';
+import './config.js';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
