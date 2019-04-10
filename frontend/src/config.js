dc1b4d41731fc9561a72ef5cc65826ac74fd29b4
Chibuzor Ogbu <chibuzorogbu@gmail.com>
2019-04-10 15:44:45 +0100

    frontend env updates

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 // Global Config file
 require('dotenv').config();
 
-global.apiUrl = process.env.API_URL;
+global.apiUrl = process.env.VUE_APP_API_URL;
 global.pusher = {
     key: process.env.VUE_APP_PUSHER_KEY,
     cluster: process.env.VUE_APP_PUSHER_CLUSTER
